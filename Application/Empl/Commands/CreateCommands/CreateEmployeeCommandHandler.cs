281fbafbe080b02a87f1c1e0ab86808381bd7004
admin <admin@DESKTOP-3HUEEDQ>
2023-05-30 10:08:31 +0600

    addRegistrToken

--- Application/Empl/Commands/CreateCommands/CreateEmployeeCommandHandler.cs
+++ Application/Empl/Commands/CreateCommands/CreateEmployeeCommandHandler.cs
@@ -1,8 +1,14 @@
 ﻿using Application.Interfaces;
+using Application.JWT;
 using Application.Services;
 using Domain.Responses;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Employee = Domain.Entities.Employee;
 
 namespace Application.Empl.Commands.CreateCommands
@@ -11,11 +17,14 @@ namespace Application.Empl.Commands.CreateCommands
     {
         private readonly IEmployeeDbContext _dbContext;
         private readonly EmailService _emailService;
+        private readonly JwtSettings _jwtSettings;
 
-        public RegisterEmployeeHandler(IEmployeeDbContext dbContext, EmailService emailService)
+        public RegisterEmployeeHandler(IEmployeeDbContext dbContext, EmailService emailService,
+            IOptions<JwtSettings> jwtSettings)
         {
             _dbContext = dbContext;
             _emailService = emailService;
+            _jwtSettings = jwtSettings.Value;
         }
 
         public async Task<Response> Handle(RegisterEmployeeCommand command, CancellationToken cancellationToken)
@@ -44,7 +53,35 @@ namespace Application.Empl.Commands.CreateCommands
             await _dbContext.SaveChangesAsync(cancellationToken);
             await _emailService.SendEmailAsync(command.Email);
 
-            return new Response(200, "Employee added successfully", true);
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, emp.Id.ToString()),
+
+            };
+
+            var token = GenerateJwtToken(claims);
+
+            return new Response(200, "Employee added successfully", true, token);
+        }
+
+        private string GenerateJwtToken(IEnumerable<Claim> claims)
+        {
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.SecretKey);
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.UtcNow.AddDays(7);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = expires,
+                SigningCredentials = credentials,
+                Issuer = _jwtSettings.Issuer,
+                Audience = _jwtSettings.Audience
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
