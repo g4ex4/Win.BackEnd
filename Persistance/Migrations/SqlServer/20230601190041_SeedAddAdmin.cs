0594a2370a29705c5157eef3f4305e391cd75f39
admin <admin@DESKTOP-3HUEEDQ>
2023-06-02 01:50:59 +0600

    AddSeedAdmin

---
+++ Persistance/Migrations/SqlServer/20230601190041_SeedAddAdmin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistance.Migrations.SqlServer
+{
+    /// <inheritdoc />
+    public partial class SeedAddAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+        table: "Employees",
+        columns: new[] { "Id", "UserName", "Email", "PasswordHash", "EmailConfirmed", "PhoneNumberConfirmed", "TwoFactorEnabled", "LockoutEnabled", "AccessFailedCount", "IsConfirmed", "JobTitle", "Experience", "Education", "DateTimeAdded", "DateTimeUpdated", "RoleId" },
+        values: new object[] { 100, "Admin", "1goldyshsergei1@gmail.com", "AQAAAAEAACcQAAAAEN5fWHtswlk5nb29qq19SUqB1JKlHcJt/dtzKi+7g8xYmrxBOJ5VyRUtTUaaOu9aGw==", true, true, true, true, 0, true, "Administrator", "10 Year", "IT Academy", DateTime.Now, DateTime.Now, 1 });
+        }
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
